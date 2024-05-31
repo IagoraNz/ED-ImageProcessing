f47063128f5f42bae6a082104c554a89e9cae8e2
cristinaadms <cristinadms07@hotmail.com>
2024-05-31 18:13:26 -0300

    Create imagegray e free imagegray

--- image.c
+++ image.c
@@ -1,3 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "image.h"
+
+ImageGray *create_image_gray(const ImageRGB *imagemrgb){
+
+    ImageGray *image = (ImageGray*)malloc(sizeof(ImageGray));
+    if(image==NULL){
+        printf("ERRO NA ALOCACAO DA MEMORIA DA IMAGE GRAY.");
+        return NULL;
+    }
+
+    image->dim.altura=imagemrgb->dim.altura;
+    image->dim.largura=imagemrgb->dim.largura;
+
+    image->pixels = (PixelGray*)malloc(image->dim.largura*image->dim.altura*sizeof(PixelGray));
+    if(image->pixels==NULL){
+        free(image);
+        return NULL;
+    }
+
+    for(int x=0; x<image->dim.altura; x++){
+        for(int y=0; y<image->dim.largura; y++){
+            int idx = x*image->dim.largura+y;
+            int resultado = (imagemrgb->pixels[idx].red+imagemrgb->pixels[idx].green+imagemrgb->pixels[idx].blue) / 3;
+            image->pixels[idx].value = resultado;
+        }
+    }
+
+    return image;
+
+}
+
+void free_image_gray(ImageGray *image){
+    if(image != NULL){
+        free(image->pixels);
+        free(image);
+    }
+}
